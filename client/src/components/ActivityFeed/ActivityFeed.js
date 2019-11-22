1d79601779758a0b3883ce83b15a557b761bd3d0
Jeffrey Russom <jeffreyrussom@gmail.com>
2019-11-22 15:52:35 -0600

    Hook up graphql to the front end

--- client/src/components/ActivityFeed/ActivityFeed.js
+++ client/src/components/ActivityFeed/ActivityFeed.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+const RECENT_SESSIONS = gql`
+  {
+    sessions {
+      start
+      end
+      project
+      activity
+    }
+  }
+`;
 
 const ActivityFeed = () => {
-  return (
-    <div>
-      <h1>Hello</h1>
+  const { loading, error, data } = useQuery(RECENT_SESSIONS);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error :(</div>;
+
+  return data.sessions.map(({ start, end, project, activity }, index) => (
+    <div key={index}>
+      <p>{`${start} ${end} ${project} ${activity}`}</p>
     </div>
-  );
+  ));
 };
 
 export default ActivityFeed;
