9c64cde94669b2a44f35461a10ca339ece8fbe6a
Jeffrey Russom <jeffreyrussom@gmail.com>
2019-11-22 16:23:34 -0600

    Update port for Heroku

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,6 @@ const schema = require("./schema");
 
 const server = new ApolloServer(schema);
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
