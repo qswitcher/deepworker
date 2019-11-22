0e91e1f8488a39fe4c81a6c13922a770752fb8da
Jeffrey Russom <jeffreyrussom@gmail.com>
2019-11-22 16:43:30 -0600

    Fix deployment

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,11 @@
 const { ApolloServer } = require("apollo-server");
 const schema = require("./schema");
 
-const server = new ApolloServer(schema);
+const server = new ApolloServer({
+  ...schema,
+  introspection: true, // enables introspection of the schema
+  playground: true // enables the actual playground
+});
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
