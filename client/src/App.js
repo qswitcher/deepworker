440dca932584073343e9d871c17cb7cd03ccd320
Jeffrey Russom <jeffreyrussom@gmail.com>
2019-11-24 14:45:22 -0600

    Style client more

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
 import "./App.css";
-import MainContent from "./components/MainContent/MainContent";
+import Composer from "./components/Composer/Composer";
+import ActivityFeed from "./components/ActivityFeed/ActivityFeed";
+import styled from "styled-components";
+
+const HeaderSection = styled.nav`
+  position: fixed;
+  width: 100%;
+  top: 0;
+  background: white;
+`;
+
+const ContentSection = styled.nav`
+  margin-top: 66px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <MainContent />
+    <div>
+      <HeaderSection>
+        <Composer />
+      </HeaderSection>
+      <ContentSection>
+        <ActivityFeed />
+      </ContentSection>
     </div>
   );
 }
--- client/src/components/ActivityFeed/ActivityFeed.js
+++ client/src/components/ActivityFeed/ActivityFeed.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
+import styled from "styled-components";
 
 const RECENT_SESSIONS = gql`
   {
@@ -13,20 +14,100 @@ const RECENT_SESSIONS = gql`
   }
 `;
 
+const FlexRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 0 10px;
+  height: 50px;
+  align-items: center;
+`;
+
+const DayGroupTitle = styled(FlexRow)`
+  font-weight: bold;
+`;
+
+const Session = styled(FlexRow)`
+  box-shadow: rgb(232, 232, 232) 0px -1px 0px 0px inset;
+  &:hover {
+    background-color: rgb(250, 251, 252);
+  }
+`;
+
+const FlexItem = styled.div`
+  padding: 0 10px;
+`;
+
+const DayGroup = styled.div`
+  background: white;
+  & + * {
+    margin-top: 30px;
+  }
+`;
+
+const SessionActivity = styled(FlexItem)`
+  flex-grow: 2;
+  display: flex;
+  align-items: center;
+  color: rgb(137, 0, 0);
+  &::before {
+    margin: 0px 5px 0px 0px;
+    display: inline-block;
+    line-height: 1;
+    content: "•";
+    font-family: Arial, sans-serif;
+    font-weight: 700;
+    font-size: 26px;
+  }
+`;
+
+const ActivityFeedContainer = styled.div`
+  background: #fafbfc;
+  height: 100%;
+  padding: 30px 0;
+`;
+
 const ActivityFeed = () => {
   const { loading, error, data } = useQuery(RECENT_SESSIONS);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error :(</div>;
 
-  return data.sessions.map(({ start, end, project, activity }, index) => (
-    <div>
-      <h1>Activity</h1>
-      <div key={index}>
-        <p>{`${start} ${end} ${project} ${activity}`}</p>
-      </div>
-    </div>
-  ));
+  // group by day
+  const dayGroups = data.sessions.reduce((acc, session) => {
+    const date = new Date(session.start).toLocaleDateString("en-US", {
+      weekday: "short",
+      day: "numeric",
+      month: "short"
+    });
+
+    acc[date] = acc[date] || [];
+    acc[date].push(session);
+    return acc;
+  }, {});
+
+  return (
+    <ActivityFeedContainer>
+      {Object.keys(dayGroups).map(date => {
+        return (
+          <DayGroup key={date}>
+            <DayGroupTitle>
+              <FlexItem>{date}</FlexItem>
+            </DayGroupTitle>
+            {dayGroups[date].map((session, index) => {
+              return (
+                <Session key={index}>
+                  <FlexItem>{session.project}</FlexItem>
+                  <SessionActivity>{session.activity}</SessionActivity>
+                  <FlexItem>{session.start}</FlexItem>
+                  <FlexItem>0</FlexItem>
+                </Session>
+              );
+            })}
+          </DayGroup>
+        );
+      })}
+    </ActivityFeedContainer>
+  );
 };
 
 export default ActivityFeed;
